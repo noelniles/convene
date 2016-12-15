db30b64717b7b8c2706a88cbb2c05ad7b2ceb29f
Noel Niles <noelniles@gmail.com>
2016-12-14 20:57:05 -1000

    Initial commit

---
+++ agent.py
@@ -0,0 +1,10 @@
+""" agent.py
+
+This class represents an agent that has somewhere to convene with other
+agents. Each agent shall have their own speed and their own view of the
+world (map).
+"""
+class agent:
+    def __init__(self):
+        self.speed = 0
+        self.world = None       # Map of the world
---
+++ convene.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" convene.py
+
+This is a driver used to simulate multiple agents that have a common 
+destination to convene.
+"""
+import googlemaps
+
+
+if __name__ == '__main__":
+    pass
