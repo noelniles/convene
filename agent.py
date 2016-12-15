fb6e7ea2281edc19f948588d54e8b06ff3f1c040
Noel Niles <noelniles@gmail.com>
2016-12-15 09:05:00 -1000

    Give each agent a unique id

--- agent.py
+++ agent.py
@@ -5,6 +5,8 @@ agents. Each agent shall have their own speed and their own view of the
 world (map).
 """
 class agent:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.speed = 0
         self.world = None       # Map of the world
+        self.start = None       # Starting point
--- convene.py
+++ convene.py
@@ -5,7 +5,19 @@ This is a driver used to simulate multiple agents that have a common
 destination to convene.
 """
 import googlemaps
+import agent
 
 
-if __name__ == '__main__":
-    pass
+def namer():
+    n = 0
+    while True:
+        yield n
+        n += 1
+
+
+if __name__ == '__main__':
+    n = namer()
+    a1 = agent.agent(next(n))
+    a2 = agent.agent(next(n))
+    print('A1: ', a1.name)
+    print('A2: ', a2.name)
