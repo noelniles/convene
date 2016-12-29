e042f7610a059026675f7af31921c81e19fea671
Noel <noelniles@gmail.com>
2016-12-28 18:40:24 -1000

    Reorganize

---
+++ config.py
@@ -0,0 +1,4 @@
+ADDRS       = 'assets/addr.txt'         # Address file used for choosing random addresses
+MAPKEY      = 'assets/mapkey.txt'       # Google Maps API key
+LOG         = 'logs/convene.log'        # Where to store the log file
+STATS       = 'logs/convene_stats.log'  # Where the profiling log is stored
